4e6143abe381857f306788ff047728c04ce50324
Bruno Teixeira <btex88@gmail.com>
2021-10-26 15:23:23 -0300

    Add pokemon img to html - done!

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 // DOM elements
 const appContainer = document.querySelector('.app-container');
 
+// Function to generate random number
+const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
+
 // API URLs
 const pokemonURL = `https://pokeapi.co/api/v2/pokemon/${rand(0, 152)}/`
 const API_URL = `https://pokeapi.co/api/v2/pokemon?limit=151`
 
-// Function to generate random number
-const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 
 // App main functions
@@ -14,20 +15,28 @@ const rand = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const getAllPokemons = () => {
   return fetch(API_URL)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => data);
 };
 
 // Function to get all pokemons
 const getPokemon = () => {
   return fetch(pokemonURL)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => data);
 };
 
-const showPokemon = () => {
-  const pokemonDiv = createElement('div')
-  pokemonDiv.classlist.add('pokemon')
+const showPokemon = async () => {
+  const pokemon = await getPokemon();
+  const pokemonDiv = document.createElement('div');
+  const pokemonImg = document.createElement('img');
+  pokemonDiv.classList.add('pokemon');
+  pokemonImg.classList.add('pokemon-img');
+  pokemonImg.src = pokemon.sprites.back_shiny;
+  pokemonDiv.appendChild(pokemonImg);
+  appContainer.appendChild(pokemonDiv);
 };
 
 
-getPokemon()
+window.onload = () => {
+  showPokemon()
+}
